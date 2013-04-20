ff3457269b5d353670767b695291e6f3880b3476
diana <diana@drakohejmen.(none)>
2013-04-21 02:45:13 +0300

    Added more boxes

--- src/configure.py
+++ src/configure.py
@@ -11,4 +11,7 @@ class DeviceConfigure:
 		choices = ["Exit", "Configure"]
 		reply = easygui.buttonbox(device.get_info(), title = device.get_name(), choices = choices)
 		if cmp(reply, 'Configure') == 0:
-			reply = easygui.msgbox("Do Stuff", title = device.get_name(), ok_button = "Exit")
+			if cmp(device.get_name(), 'EthernetRJ45') == 0:
+				reply = easygui.msgbox("Your IP:", title = device.get_name())
+			else:
+				reply = easygui.msgbox("Do Stuff", title = device.get_name(), ok_button = "Exit")
