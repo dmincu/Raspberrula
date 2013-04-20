105518c16a9f0936b82b67a249fd72be2290337e
simonabadoiu <simonaandreea.badoiu@gmail.com>
2013-04-21 00:58:06 +0300

    Portal collisions

--- src/creeps_try.py
+++ src/creeps_try.py
@@ -14,10 +14,13 @@ class MoveRasp(Sprite):
 		init_direction, speed, devices):
 		
 		Sprite.__init__(self)
+		self.portal_collision = []
 		self.collision = 0
 		self.screen = screen
 		self.speed = speed
 		self.devices = devices.my_list
+		for device in self.devices:
+			self.portal_collision.append([0, device])
 		self.base_image = pygame.image.load(img_filename).convert_alpha()
 		self.size = (self.base_image.get_width(), self.base_image.get_height())
 		self.base_image = pygame.transform.scale(self.base_image, self.size)
@@ -30,31 +33,53 @@ class MoveRasp(Sprite):
 
 	def collisions(self):
 		self.collision = 0
+		i = 0
 		for device in self.devices:
 			upper_limit = device.get_upper()
 			lower_limit = device.get_lower()
-			my_pos = self.pos
 			
+			portal_x = (device.get_portal_coord())[0]
+			portal_y = (device.get_portal_coord())[1]
+
+			my_pos = self.pos
+			print self.pos, upper_limit, lower_limit, device.get_name()
+			
+			print device.get_name() + " " + 'PLACA' + "\n"
+			print cmp(device.get_name(), 'PLACA') == 0
 			if (cmp(device.get_name(), 'PLACA') == 0):
+				print "mori"
+				print device.get_name()
 				if (	(self.pos.x < upper_limit[0]
 				    or self.pos.x > lower_limit[0])
 				    or (self.pos.y < upper_limit[1]
 				    or self.pos.y > lower_limit[1])
 				   ):
 					self.collision = 1
+					print "coliziune"
 			else:
 				if (	self.pos.x > upper_limit[0]
 				    and self.pos.x < lower_limit[0]
 				    and self.pos.y > upper_limit[1]
 				    and self.pos.y < lower_limit[1]
 				   ):
-					self.collision = 1		
-
+					self.collision = 1
+					print "coliziune"
+				
+				if (    self.pos.x + 18 > portal_x
+                                    and self.pos.x + 18 < portal_x + 17
+                                    and self.pos.y + 16 > portal_y
+                                    and self.pos.y + 16 < portal_y + 18
+                                   ):
+                                        self.portal_collision[i][0] = 1
+					print "Coliziune cu ursul"
+			i += 1
+	
 	def blitme(self):
 		self.screen.blit(self.image, self.pos);	
 
 	def move_down(self):
         	""" Move the raspberry down """
+		print self.collision
 		displacement = vec2d(0, self.speed)
 		self.pos += displacement
 		self.collisions()
