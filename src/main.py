eef4b4e223cadcf0e2cc2492bc1c6dc116b60252
diana <diana@drakohejmen.(none)>
2013-04-20 14:23:47 +0300

    Added comments in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+# Raspberrula Game - Raspberry Hack
+
 import pygame, sys
 from pygame.locals import *
 
@@ -5,16 +7,19 @@ width = 800
 height = 600
 
 if __name__ == "__main__":
+	# Initialize pygame module	
 	pygame.init()
-
 	fpsClock = pygame.time.Clock()
+	
+	# Initialize background
 	size = (width, height)
 	screen = pygame.display.set_mode(size)
 	pygame.display.set_caption("Raspberrula")
-	backgroundImg = pygame.image.load("../Images/Raspberrula.Intro.Scaled.jpg").convert()
+	backgroundImg = pygame.image.load("../Images/Raspberrula Intro.jpg").convert()
 	backgroundImg = pygame.transform.scale(backgroundImg, (width, height))	
 	backgroundRect = backgroundImg.get_rect()	
 
+	# Run Game
 	while True:
 		for event in pygame.event.get():
 			if event.type == QUIT:
